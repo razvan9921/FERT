6be5062dd6dd67fd09a305d1e73b957dd542ab1f
razvan9921 <razvangabriel9921@gmail.com>
2019-01-31 16:45:52 +0200

    made responsivness great again, added courses in student

---
+++ ClientApp/public/javascript/home-student.js
@@ -0,0 +1,42 @@
+function getCourses() {
+    const app = firebase.app();
+    const db = firebase.firestore();
+    // var email = localStorage.getItem('email');
+    var courses = db.collection('courses');
+  
+  
+    // courses = courses.where('createdBy', '==', email);
+    courses.onSnapshot(querySnapshot => {
+      var coursesDOM = document.getElementById('courses');
+      coursesDOM.innerHTML = '';
+      querySnapshot.forEach(function (doc) {
+        console.log(doc.data())
+        element = doc.data();
+        element.id = doc.id;
+        var coursesDOM = document.getElementById('courses');
+        // items.forEach(element => {
+        var courseDIV = document.createElement('div');
+        courseDIV.classList.add('course', 'display-flex', 'flex-column', 'space-between', 'margin-10', 'padding-10');
+        coursesDOM.appendChild(courseDIV);
+  
+        title = document.createElement('h4');
+        title.setAttribute('id', element.id);
+        title.setAttribute('onclick', `redirectToCourse("${element.id}")`)
+        title.innerHTML = element.title;
+        courseDIV.appendChild(title);
+  
+        detailsDIV = document.createElement('div');
+        detailsDIV.classList.add('display-flex', 'space-between', 'margin-top-5');
+        courseDIV.appendChild(detailsDIV);
+  
+        classroom = document.createElement('p');
+        classroom.innerHTML = element.classroom;
+        detailsDIV.appendChild(classroom);
+  
+        date = document.createElement('p');
+        date.classList.add('margin-left-10');
+        date.innerHTML = element.date;
+        detailsDIV.appendChild(date);
+      });
+    });
+  }
